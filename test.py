b29b3f90c3664cd6881fd67692b6b72bfb0736dc
acul <samsulrahmadani@gmail.com>
2021-07-17 03:13:54 +0700

    fix

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 from torchvision.io import ImageReadMode, read_image
 import torch
-from transformers import MarianTokenizer,ViTFeatureExtractor
+from transformers import MarianTokenizer,ViTFeatureExtractor,CLIPFeatureExtractor
 from flax_clip_vision_marian.modeling_clip_vision_marian import FlaxCLIPVisionMarianForConditionalGeneration
 flax_marian_clip = FlaxCLIPVisionMarianForConditionalGeneration.from_pretrained('munggok/image-captioning-marian',)
 from torchvision.io import ImageReadMode, read_image
 marian_model_name = 'Helsinki-NLP/opus-mt-en-id'
 tokenizer = MarianTokenizer.from_pretrained(marian_model_name)
 clip_model_name = 'openai/clip-vit-base-patch32'
-feature_extractor = ViTFeatureExtractor.from_pretrained(clip_model_name)
+feature_extractor = CLIPFeatureExtractor.from_pretrained(clip_model_name)
 
 
 max_length = 64
@@ -23,7 +23,7 @@ image = read_image('000000039769.jpg', mode=ImageReadMode.RGB)
 encoder_inputs = feature_extractor(images=image, return_tensors="jax")
 pixel_values = encoder_inputs.pixel_values
 
-pixel_values = torch.stack([image]).permute(0, 2, 3, 1).numpy()
+#pixel_values = torch.stack([image]).permute(0, 2, 3, 1).numpy()
 
 
 batch = {'pixel_values': pixel_values}
