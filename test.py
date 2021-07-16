f49e10ba9e25d9999cddb7b55da0f94ba2d5676a
acul <samsulrahmadani@gmail.com>
2021-07-17 03:10:15 +0700

    fix

--- test.py
+++ test.py
@@ -1,11 +1,14 @@
 from torchvision.io import ImageReadMode, read_image
 import torch
-from transformers import MarianTokenizer
+from transformers import MarianTokenizer,ViTFeatureExtractor
 from flax_clip_vision_marian.modeling_clip_vision_marian import FlaxCLIPVisionMarianForConditionalGeneration
 flax_marian_clip = FlaxCLIPVisionMarianForConditionalGeneration.from_pretrained('munggok/image-captioning-marian',)
 from torchvision.io import ImageReadMode, read_image
 marian_model_name = 'Helsinki-NLP/opus-mt-en-id'
 tokenizer = MarianTokenizer.from_pretrained(marian_model_name)
+clip_model_name = 'openai/clip-vit-base-patch32'
+feature_extractor = ViTFeatureExtractor.from_pretrained(clip_model_name)
+
 
 max_length = 64
 num_beams = 4
@@ -16,6 +19,9 @@ def generate_step(batch):
     return output_ids.sequences
 
 image = read_image('000000039769.jpg', mode=ImageReadMode.RGB)
+
+encoder_inputs = feature_extractor(images=image, return_tensors="jax")
+pixel_values = encoder_inputs.pixel_values
 
 pixel_values = torch.stack([image]).permute(0, 2, 3, 1).numpy()
 
